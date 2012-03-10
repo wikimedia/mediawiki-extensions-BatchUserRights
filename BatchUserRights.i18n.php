5562412688babac4a83d0e3352184f842fa52d45
Raimond Spekking <raymond@users.mediawiki.org>
2012-03-10 22:57:43 +0000

    Localisation updates for core and extension messages from translatewiki.net

--- BatchUserRights.i18n.php
+++ BatchUserRights.i18n.php
@@ -395,6 +395,25 @@ Nu wordt er niets uitgevoerd.
 De rest van de pagina kan uitgevoerd worden zodat je eenvoudig kunt zien of een van de gebruikersnamen niet geladen kon worden.',
 );
 
+/** Polish (Polski)
+ * @author BeginaFelicysym
+ */
+$messages['pl'] = array(
+	'batchuserrights' => 'Wsadowe uprawnienia użytkowników',
+	'batchuserrights-desc' => 'Umożliwia dodawanie jednego lub więcej użytkowników do grupy lub kilku grup w ramach jednej akcji',
+	'batchuserrights-names' => 'Nazwy użytkowników do dodania do tej grupy (po jednej w wierszu):',
+	'batchuserrights-intro' => 'Ta strona pozwoli Ci dodać grupę do wielu użytkowników naraz.
+Ze względów bezpieczeństwa lista możliwych do dodania grup jest ustawiona w konfiguracji rozszerzenia i nie można jej zmieniać w wiki.
+Poproś administratora systemu jeśli należy zezwolić na dodawanie wsadowe innych grup.',
+	'batchuserrights-single-progress-update' => 'Dodano {{PLURAL:$1| grupę|grupy|grupy}} do <strong>$2</strong>.',
+	'batchuserrights-add-groups' => 'Dodawanie {{PLURAL:$1|jednego użytkownika|$1 użytkowników}} do {{PLURAL:$2|następującej grupy|następujących grup}}: <strong>$3</strong>.',
+	'batchuserrights-userload-error' => "Nie można wczytać użytkownika \"'''\$1'''\".",
+	'batchuserrights-no-groups' => 'Nie wybrano żadnych grup.
+Wykonanie nie spowoduje żadnych zmian.
+Pozostałą część strony będzie można uruchomić tylko po to, by dało się zobaczyć, czy wszystkie nazwy uzytkownikow mogą być załadowane.',
+	'right-batchuserrights' => 'Dodawanie jednego lub więcej użytkowników do grupy lub kilku grup w ramach jednej akcji',
+);
+
 /** Piedmontese (Piemontèis)
  * @author Borichèt
  * @author Dragonòt
